149f49ec4f0081b8c16cb34893375adcf4b9459e
Travis Lawrence <tlawrence@tumblr.com>
2017-07-10 10:53:49 -0700

    Create better sentences

--- public/js/application.js
+++ public/js/application.js
@@ -1,63 +1,57 @@
-const words = [
-    "somebody", "once", "told", "me", "the", "world", "is", "gonna", "roll", "me",
-    "ain't", "the", "sharpest", "tool", "in", "the", "shed", "she", "was",
-    "looking", "kind", "of", "dumb", "with", "her", "finger", "and", "her", "thumb",
-    "in", "the", "shape", "of", "an", "on", "her", "forehead", "well", "the",
-    "years", "start", "coming", "and", "they", "don't", "stop", "coming", "fed", "to",
-    "the", "rules", "and", "I", "hit", "the", "ground", "running", "didn't",
-    "make", "sense", "not", "to", "live", "for", "fun", "your", "brain", "gets",
-    "smart", "but", "your", "head", "gets", "dumb", "so", "much", "to", "do", "so"
-];
+const verbs = ['create', 'build', 'invent', 'run', 'jump', 'skip'];
+const adjectives = ['good', 'bad', 'fat', 'skinny', 'tall', 'short', 'gross', 'happy'];
+const pluralNouns = ['cats', 'dogs', 'frogs', 'stars', 'songs', 'projects'];
 
 const bodyEl = document.querySelector('.container');
-let lastPhrase;
-
-function randomizer (max, min) {
-    min = min || 0;
-    max = max || 1;
-
-    return Math.round(Math.random() * (max - min) + min);
-}
-
-function titleCase (text) {
-    return text.slice(0,1).toUpperCase() + text.slice(1);
-}
-
-function savePhrase (phrase) {
-    let el = document.createElement('div');
-    el.classList = 'previous-phrase';
-    el.innerHTML = `<h3>${phrase}</h3>`;
-
-    el.style.top = randomizer(90, 10) + '%';
-    el.style.left = randomizer(90, 10) + '%';
-
-    bodyEl.appendChild(el);
-}
-
-function phraseGenerator () {
-    const phraseLength = 3;
-    let oath = [];
-    for (let i = 0; i < phraseLength; i++) {
-        oath.push(words[randomizer(words.length - 1)]);
-    }
-    let phrase = oath.join(' ');
-    return titleCase(phrase);
-}
-
-function buildOath (el) {
-    textEl = document.getElementById(el);
-    const phrase = phraseGenerator();
-    textEl.textContent = phrase;
-    savePhrase(phrase);
-}
 
 class OathGenerator {
 
     constructor (buttonEl) {
         this.buttonEl = document.getElementById(buttonEl);
         this.buttonEl.addEventListener('click', (e) => {
-            buildOath('your-oath');
+            this.buildOath('your-oath');
         });
+    }
+
+    randomizer (max, min) {
+        min = min || 0;
+        max = max || 1;
+
+        return Math.round(Math.random() * (max - min) + min);
+    }
+
+    titleCase (text) {
+        return text.slice(0,1).toUpperCase() + text.slice(1);
+    }
+
+    savePhrase (phrase) {
+        let el = document.createElement('div');
+        el.classList = 'previous-phrase';
+        el.innerHTML = `<h3>${phrase}</h3>`;
+
+        el.style.top = this.randomizer(90, 10) + '%';
+        el.style.left = this.randomizer(90, 10) + '%';
+
+        bodyEl.appendChild(el);
+    }
+
+    phraseGenerator () {
+        const phraseLength = 3;
+        let oath = [];
+        
+        oath.push(verbs[this.randomizer(verbs.length - 1)]);
+        oath.push(adjectives[this.randomizer(adjectives.length - 1)]);
+        oath.push(pluralNouns[this.randomizer(pluralNouns.length - 1)]);
+
+        let phrase = oath.join(' ');
+        return this.titleCase(phrase);
+    }
+
+    buildOath (el) {
+        let textEl = document.getElementById(el);
+        const phrase = this.phraseGenerator();
+        textEl.textContent = phrase;
+        this.savePhrase(phrase);
     }
 
 }
